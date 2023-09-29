c24e0e28257cf0f625d1913f14c7542a65473547
Valeriya Belyayeva <848belval848@gmail.com>
2023-09-30 02:18:31 +0300

    lesson 44

---
+++ chat/views.py
@@ -0,0 +1,10 @@
+# from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+
+class Room(TemplateView):
+    template_name = 'room.html'
